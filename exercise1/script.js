fd7ab77d2491e3f57ff1525dae0105ad44fd6bf6
esthergoldman <77267248+esthergoldman@users.noreply.github.com>
2021-01-17 15:04:42 +0200

    Add files via upload

---
+++ exercise1/script.js
@@ -0,0 +1,83 @@
+//5+"34"
+
+let a=5;
+let b="34";
+let c= a+b;
+console.log(c);
+
+//5-"4"
+let d=5;
+let e="4";
+let f=d-e;
+console.log(f);
+
+//ghijklmnopqrs
+
+//10 % 5
+let g=10;
+let h=5;
+let i=g%h;
+console.log(i)
+
+
+//5 % 10
+let j=5;
+let k=10;
+let l=j%k;
+console.log(l)
+
+
+//"Java" + "Script"
+let m="java";
+let n="Script";
+let o=m+n;
+console.log(o)
+
+
+//" " + " "
+let p= " ";
+let q= " ";
+let r= p+q;
+console.log(r)
+
+//" " + 0
+let s= " ";
+let t= 0;
+let u= s+t;
+console.log(u)
+
+//true + true
+let v= true;
+let w= true;
+let x= v+w;
+console.log(x)
+
+//true + false
+let y= true; 
+let z= false;
+let aa= y+z;
+console.log(aa)
+
+//false + true
+let bb=false;
+let cc= true;
+let dd= bb+cc;
+console.log(dd)
+
+//false - true
+let ee= false;
+let ff=true;
+let gg= ee-f;
+console.log(gg)
+
+//3 - 4
+let hh=3;
+let ii=4;
+let jj= hh-4;
+console.log(jj)
+
+//"Bob" - "bill"
+let kk="bob"
+let ll= "bill"
+let mmm= kk-ll;
+console.log(mmm)
